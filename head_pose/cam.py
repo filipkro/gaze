8f19a16266f8262cd96aae1a139091425266ad42
JosefAbdulAl <josef.abdulal@gmail.com>
2021-03-18 10:30:42 +0100

    cam

--- head_pose/cam.py
+++ head_pose/cam.py
@@ -6,22 +6,22 @@ import numpy as np
 from calc_normal_3d import find_normal
 
 def draw(image, face):
-   bbox = face['box']
-   cv2.putText(image,str(np.round(face['confidence'],2)),(int(np.round(bbox[0])),int(np.round(bbox[1]))),cv2.FONT_HERSHEY_TRIPLEX,1,color=(255,0,255))
-   cv2.rectangle(image, (int(np.round(bbox[0])),int(np.round(bbox[1]))),(int(np.round(bbox[2]+bbox[0])),int(np.round(bbox[3]+bbox[1]))),(0,0,255))
+    bbox = face['box']
+    cv2.putText(image,str(np.round(face['confidence'],2)),(int(np.round(bbox[0])),int(np.round(bbox[1]))),cv2.FONT_HERSHEY_TRIPLEX,1,color=(255,0,255))
+    cv2.rectangle(image, (int(np.round(bbox[0])),int(np.round(bbox[1]))),(int(np.round(bbox[2]+bbox[0])),int(np.round(bbox[3]+bbox[1]))),(0,0,255))
 
-   for value in face['keypoints'].items():
-      cv2.circle(image, (int(np.round(value[1][0])),int(np.round(value[1][1]))), 3, (0,0,255))
-   return image
+    for value in face['keypoints'].items():
+        cv2.circle(image, (int(np.round(value[1][0])),int(np.round(value[1][1]))), 3, (0,0,255))
+    return image
 
 def draw_normal(image, normal, face):
-   if normal[0] == float('nan') or normal[1] == float('nan'):
-    print("Error!!")
-   p1 = face['keypoints']['nose']
-   p2 = (int(np.round(p1[0]+normal[0])), int(np.round(p1[1]+normal[1])))
+    if normal[0] == float('nan') or normal[1] == float('nan'):
+        print("Error!!")
+    p1 = face['keypoints']['nose']
+    p2 = (int(np.round(p1[0]+normal[0])), int(np.round(p1[1]+normal[1])))
 
-   cv2.line(image, p1, p2, (255,0,0), 2)
-   return image
+    cv2.line(image, p1, p2, (255,0,0), 2)
+    return image
 
 
 if __name__ == '__main__':
@@ -43,6 +43,15 @@ if __name__ == '__main__':
         
         # detect faces in the image
         faces = detector.detect_faces(frame)
+        
+        while len(faces) == 0:
+            ret, frame = vid.read()
+            faces = detector.detect_faces(frame)
+            frame = cv2.flip(frame, 1)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
         for face in faces:
             image = draw(frame, face)
             normal = find_normal(face['keypoints'],image)
