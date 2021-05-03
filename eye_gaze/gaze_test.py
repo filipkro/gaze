bdab99ee72d182c25f7aae813e1fc74b7054f0d4
JosefAbdulAl <josef.abdulal@gmail.com>
2021-05-03 10:37:46 +0200

    05-03

--- eye_gaze/gaze_test.py
+++ eye_gaze/gaze_test.py
@@ -27,6 +27,53 @@ def crop_image(image, eye_center, size):
     
     return cropped_image
 
+def find_normal(landmarks):
+    left_eye = np.array(landmarks['left_eye'])
+    right_eye = np.array(landmarks['right_eye'])
+    eye_vector = right_eye-left_eye
+    eye_mid = left_eye + eye_vector/2
+
+
+    mouth_left = np.array(landmarks['mouth_left'])
+    mouth_right = np.array(landmarks['mouth_right'])
+    mouth_vector = mouth_right-mouth_left
+    mouth_mid = mouth_left + mouth_vector/2
+
+    Rm = 0.5
+    a_vector = mouth_mid - eye_mid
+    nose_base = np.round(eye_mid + a_vector*(1-Rm))
+    projected_normal = np.array(landmarks['nose']-nose_base)
+    ln = np.linalg.norm(projected_normal)
+    lf = np.linalg.norm(a_vector)
+
+    l1 = max(np.linalg.norm(projected_normal),0.01)
+    l2 = max(np.linalg.norm(projected_normal)*np.linalg.norm(a_vector),0.01)
+    
+    tau = np.arccos(np.dot(projected_normal,(1,0))/l1)
+    if projected_normal[1]<0:
+        tau = 2*np.pi-tau
+    theta = np.arccos(np.dot(projected_normal,-a_vector)/l2)
+
+    p1 = landmarks['nose']
+    p2 = landmarks['nose']+projected_normal
+    p2 = (int(p2[0]),int(p2[1]))
+
+    m1 = (ln/lf)**2
+    m2 = np.cos(theta)**2
+    if m2==1:
+        m2 *= 0.99
+    Rn = 0.6
+
+    x1 = -(m1-Rn**2+2*m2*Rn**2)/(2*Rn**2*(1-m2))
+    x2 = np.sqrt(((m1-Rn**2+2*m2*Rn**2)/(2*Rn**2*(1-m2)))**2+(m2*Rn**2)/(Rn**2*(1-m2)))
+    dz = np.sqrt(x1+x2)
+
+    sigma = np.arccos(np.abs(dz))
+    normal = np.array([np.sin(sigma)*np.cos(tau), np.sin(sigma)*np.sin(tau), -np.cos(sigma)])
+    normal /=np.linalg.norm(normal)
+
+    return normal
+
 
 if __name__ == '__main__':
 
@@ -34,7 +81,7 @@ if __name__ == '__main__':
     detector = MTCNN()
 
     # define a video capture object 
-    vid = cv2.VideoCapture(1) 
+    vid = cv2.VideoCapture(0) 
     counter = 0
     
     while(True): 
@@ -81,8 +128,12 @@ if __name__ == '__main__':
                 right_dps[0] = int(round(right_dps[0]))
                 right_dps[1] = int(round(right_dps[1]))
 
-                frame = draw_normal(frame, 50*left_dps, left_eye)
-                frame = draw_normal(frame, 50*right_dps, right_eye)
+                normal = find_normal(keypoints)
+                gaze_left = left_dps + normal
+                gaze_right = right_dps + normal
+
+                frame = draw_normal(frame, 10*gaze_left, left_eye)
+                frame = draw_normal(frame, 10*gaze_right, right_eye)
                 # Display the resulting frame 
                 cv2.imshow('frame', frame) 
 
